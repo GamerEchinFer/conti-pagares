4d24ec83d7324c3190c5f9990b4d7c8101301537
Cristina Almeida <maria.almeida@bancontinental.com.py>
2022-11-08 08:11:36 -0300

    migración de elementos assets y otros

---
+++ pages/solicitud/index.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material"
+
+const SolicitudPage = () => {
+    return (
+        <Typography variant="h1">Tipo Busqueda</Typography>
+    )
+}
+
+export default SolicitudPage
---
+++ pages/subirDocumento/index.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material"
+
+const SubirDocumentoPage = () => {
+    return (
+        <Typography variant="h1">Subir Documento</Typography>
+    )
+}
+
+export default SubirDocumentoPage
