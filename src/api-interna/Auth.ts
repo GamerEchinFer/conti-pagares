9e47deb79b9398fd930a04b03aec7a9facdc31e0
Cristina Almeida <maria.almeida@bancontinental.com.py>
2023-07-11 11:00:38 -0400

    bibliotecas de importacion de models

--- src/api-interna/Auth.ts
+++ src/api-interna/Auth.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { ConfigApiInterna } from "../config/config";
-import { DatosAgenteResponse, LoginResponse, PermisosUsuarioResponse } from "../models/responses";
-import IpGeolocationResponse from "../models/responses/IpGeolocation.response";
+import { DatosAgenteResponse, IpGeolocationResponse, LoginResponse, PermisosUsuarioResponse } from "../models/responses";
 
 const { auth } = ConfigApiInterna;
 
