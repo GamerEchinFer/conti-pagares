d81af379a8741ab659fa5dac71c8df58beabca10
FernandoAlfonso <gamerechin@gmail.com>
2023-12-01 08:47:27 -0300

    cambio a api custodia .env

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,19 +10,19 @@ export const api = axios.create(
 
 export const authApi = axios.create(
     {
-        baseURL: process.env.NEXT_PUBLIC_API_BASE_URL+"/autenticarServicio/v1/realms/interno"
+        baseURL: process.env.NEXT_PUBLIC_API_CUSTODIA_URL+"/autenticarServicio/v1/realms/interno"
     }
 );
     
 export const apmApi = axios.create(
     {
-        baseURL: process.env.NEXT_PUBLIC_API_BASE_URL+"/gestion-documental-interno/v1"
+        baseURL: process.env.NEXT_PUBLIC_API_CUSTODIA_URL+"/gestion-documental-interno/v1"
     }
 );
 
 export const apmApiCliente = axios.create(
     {
-        baseURL: process.env.NEXT_PUBLIC_API_BASE_URL+"/interno/clientes/datos/v1"
+        baseURL: process.env.NEXT_PUBLIC_API_CUSTODIA_URL+"/interno/clientes/datos/v1"
     }
 );
 
@@ -34,7 +34,7 @@ export const apmApiHadoop = axios.create(
 
 export const apmApiMsFileStream = axios.create(
     {
-        baseURL: process.env.NEXT_PUBLIC_API_BASE_URL+"/ms-file-stream-manager/v1"
+        baseURL: process.env.NEXT_PUBLIC_API_CUSTODIA_URL+"/ms-file-stream-manager/v1"
     }
 );
 
