4e68f6fc735bdd5d84b857b92666e46cd92ad213
FernandoAlfonso <gamerechin@gmail.com>
2023-12-05 16:56:33 -0300

    Refactor FormActions component and generate status history

--- src/components/promissoryNotes/consult/Form.tsx
+++ src/components/promissoryNotes/consult/Form.tsx
@@ -6,74 +6,16 @@ import DetailDataTable from './DetailDataTable'
 import DeliveryComment from './DeliveryComment'
 import { PromissoryNotesConsult } from '../../../models/responses/promissoryNotes'
 import { EPromissoryNotesStatus, PromissoryNotesStatusHistory } from '../../../interfaces/promissoryNotes'
+import { promissoryNotesHelper } from '../../../helpers/promissoryNotes'
 
 interface FormProps {
     promissoryNotesConsult: PromissoryNotesConsult | null;
 }
 const Form = ({ promissoryNotesConsult }: FormProps) => {
-    const statusHistory: PromissoryNotesStatusHistory[] = [];
+    const statusHistory: PromissoryNotesStatusHistory[] = promissoryNotesConsult ?
+        promissoryNotesHelper.generateStatusHistory(promissoryNotesConsult) : [];
 
-    const keys = Object.keys(promissoryNotesConsult ?? {});
-    if (keys.length > 0) {
-        keys.forEach(item => {
-            if (item == EPromissoryNotesStatus.Request && promissoryNotesConsult?.solicitud?.fechaSolicitud) {
-                statusHistory.push({
-                    branch: promissoryNotesConsult?.solicitud?.nombreSucursal ?? "",
-                    status: EPromissoryNotesStatus.Request,
-                    createdAt: promissoryNotesConsult?.solicitud?.fechaSolicitud ?? "",
-                    area: promissoryNotesConsult?.solicitud?.areaSolicitud ?? "",
-                    user: promissoryNotesConsult?.solicitud?.nombreUsuario ?? "",
-                })
-            }
-            if (item == EPromissoryNotesStatus.Reception && promissoryNotesConsult?.recepcion?.fechaRecepcion) {
-                statusHistory.push({
-                    branch: "",
-                    status: EPromissoryNotesStatus.Reception,
-                    createdAt: promissoryNotesConsult?.recepcion?.fechaRecepcion ?? "",
-                    area: promissoryNotesConsult?.recepcion?.areaRecepcion ?? "",
-                    user: promissoryNotesConsult?.recepcion?.nombreUsuario ?? "",
-                })
-            }
-            if (item == EPromissoryNotesStatus.Shipment && promissoryNotesConsult?.envio?.fechaEnvio) {
-                statusHistory.push({
-                    status: EPromissoryNotesStatus.Shipment,
-                    createdAt: promissoryNotesConsult?.envio?.fechaEnvio ?? "",
-                    area: promissoryNotesConsult?.envio?.areaEnvio ?? "",
-                    user: promissoryNotesConsult?.envio?.nombreUsuario ?? "",
-                })
-            }
-            if (item == EPromissoryNotesStatus.Return && promissoryNotesConsult?.devolucion?.fechaDevolucion) {
-                statusHistory.push({
-                    status: EPromissoryNotesStatus.Return,
-                    createdAt: promissoryNotesConsult?.devolucion?.fechaDevolucion ?? "",
-                    user: promissoryNotesConsult?.devolucion?.nombreUsuario ?? "",
-                })
-            }
-            if (item == EPromissoryNotesStatus.ReceptionCust && promissoryNotesConsult?.recepcionCustodia?.fechaRecepcionCustodia) {
-                statusHistory.push({
-                    status: EPromissoryNotesStatus.ReceptionCust,
-                    createdAt: promissoryNotesConsult?.recepcionCustodia?.fechaRecepcionCustodia ?? "",
-                    user: promissoryNotesConsult?.recepcionCustodia?.nombreUsuario ?? "",
-                })
-            }
-            if (item == EPromissoryNotesStatus.Reject && promissoryNotesConsult?.rechazo?.fechaRechazo) {
-                statusHistory.push({
-                    status: EPromissoryNotesStatus.Reject,
-                    createdAt: promissoryNotesConsult?.rechazo?.fechaRechazo ?? "",
-                    user: promissoryNotesConsult?.rechazo?.nombreUsuario ?? "",
-                })
-            }
-            if (item == EPromissoryNotesStatus.Delivery && promissoryNotesConsult?.entrega?.fechaEntrega) {
-                statusHistory.push({
-                    status: EPromissoryNotesStatus.Delivery,
-                    createdAt: promissoryNotesConsult?.entrega?.fechaEntrega ?? "",
-                    user: promissoryNotesConsult?.entrega?.nombreUsuario ?? "",
-                })
-            }
-        });
-    }
-    
-    console.log(promissoryNotesConsult)
+
     return (
         <Stack
             spacing={3}
--- src/components/promissoryNotes/consult/FormActions.tsx
+++ src/components/promissoryNotes/consult/FormActions.tsx
@@ -1,51 +1,19 @@
-import Button from '@mui/material/Button'
 import ExportButton from '../../shared/ui/buttons/ExportButton';
 import { exportDataTable } from '../../../helpers/exportFromJson';
 import { PromissoryNotesConsult } from '../../../models/responses/promissoryNotes';
-import { EPromissoryNotesStatus, PromissoryNotesStatusHistory } from '../../../interfaces/promissoryNotes';
+import { PromissoryNotesStatusHistory } from '../../../interfaces/promissoryNotes';
+import { promissoryNotesHelper } from '../../../helpers/promissoryNotes';
 
 interface FormActionsProps {
     promissoryNotesConsult: PromissoryNotesConsult | null;
 }
 const FormActions = ({ promissoryNotesConsult }: FormActionsProps) => {
-    const statusHistory: PromissoryNotesStatusHistory[] = [];
-
-    const keys = Object.keys(promissoryNotesConsult ?? {});
-    if (keys.length > 0) {
-        keys.forEach(item => {
-            if (item == EPromissoryNotesStatus.Request) {
-                statusHistory.push({
-                    branch: promissoryNotesConsult?.solicitud?.nombreSucursal ?? "",
-                    status: EPromissoryNotesStatus.Request,
-                    createdAt: promissoryNotesConsult?.solicitud?.fechaSolicitud ?? "",
-                    area: promissoryNotesConsult?.solicitud?.areaSolicitud ?? "",
-                    user: promissoryNotesConsult?.solicitud?.nombreUsuario ?? "",
-                })
-            }
-            if (item == EPromissoryNotesStatus.Reception) {
-                statusHistory.push({
-                    branch: "",
-                    status: EPromissoryNotesStatus.Reception,
-                    createdAt: promissoryNotesConsult?.recepcion?.fechaRecepcion ?? "",
-                    area: promissoryNotesConsult?.recepcion?.areaRecepcion ?? "",
-                    user: promissoryNotesConsult?.recepcion?.nombreUsuario ?? "",
-                })
-            }
-            if (item == EPromissoryNotesStatus.Delivery) {
-                statusHistory.push({
-                    branch: "",
-                    status: EPromissoryNotesStatus.Delivery,
-                    createdAt: promissoryNotesConsult?.envio?.fechaEnvio ?? "",
-                    area: promissoryNotesConsult?.envio?.areaEnvio ?? "",
-                    user: promissoryNotesConsult?.envio?.nombreUsuario ?? "",
-                })
-            }
-        });
-    }
+    const statusHistory: PromissoryNotesStatusHistory[] = promissoryNotesConsult ?
+        promissoryNotesHelper.generateStatusHistory(promissoryNotesConsult) : [];
 
     const handlerClickExport = () => {
         if (statusHistory.length > 0) {
-            exportDataTable({ data: [promissoryNotesConsult], fileName: "example" });
+            exportDataTable({ data: statusHistory, fileName: "example" });
         }
     };
 
